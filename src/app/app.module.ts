8735fd7e8d7e245080fe521b130fc61779403cb1
Pranali jadhav <pranalidjadhav@gmail.com>
2023-12-08 21:42:44 +0530

    Update @fortawesome/angular-fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
 
@@ -21,7 +21,7 @@ import { Portfolio1Component } from './portfolio1/portfolio1.component';
     Portfolio1Component
   ],
   imports: [
-    FontAwesomeModule,
+    
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
